79c8555aae99068aed94b08f68371cf82b7691cd
Li Maoqi <lmq729981607@gmail.com>
2016-07-18 18:06:49 +0800

    供应商管理

--- js/own.js
+++ js/own.js
@@ -1,3 +1,5 @@
+
+
 function changeWarehouseKind(newKind) {
     document.getElementById("warehouse-kind-selector").innerHTML = newKind;
 }
@@ -18,3 +20,47 @@ function warehouseChangeKind(newKind) {
     caret.setAttribute("class", "caret");
     document.getElementById("warehouse-kind-selector").appendChild(caret);
 }
+
+$(document).ready(function() {
+    $(".delete-item-btn").click(function(){
+        $(this).parent().remove();
+    })
+})
+
+$(document).ready(function(){
+    $("#edit_facebox_btn").click(function(){
+        $(this).facebox();
+    })
+})
+
+$(document).ready(function(){
+    $("#add_supplier_btn").click(function(){
+        $(this).facebox();
+    })
+})
+
+$(document).ready(function(){
+    $(".des-btn").click(function(){
+        var content = "我要崩溃了！"
+        $(this).attr("data-content", content).popover();
+    })
+})
+
+$(document).ready(function(){
+    $("#add_goods_supplied").click(function(){
+        $(this).facebox();
+    })
+})
+
+// $(document).ready(function(){
+//     $("#make_sure").click(function(){
+//
+//     })
+// })
+//
+// function addSupplier(supplierName) {
+//     var list_group = document.getElementById("list_group");
+//     var newSupplierItem = document.createElement("div");
+//     newSupplierItem.setAttribute("class", "list-group-item");
+//     newSupplierItem.innerHTML = ""
+// }
